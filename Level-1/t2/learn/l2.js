b196d0c05832c844928efc3764fd3f602207842c
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-03-15 06:46:07 +0600

    done
    clear

--- Level-1/t2/learn/l2.js
+++ Level-1/t2/learn/l2.js
@@ -1,12 +1,38 @@
-/*
+var a = 10
+let b = 20
+const c = 30
 
-falsy value =>
-1.false
-2.0
-3.""   empty string
-4.null
-5.undefined
-6.NaN 
+a = 50   // ok
+b = 60   // ok
+c = 70   // ❌ error
 
-else are truty value
-*/
+
+
+// -----**-----
+
+if (true) {
+  var x = 10
+  let y = 20
+}
+
+console.log(x) // 10
+console.log(y) // ❌ error
+
+
+var aa = 10
+let bb = 20
+
+if (true) {
+  var aa = 30
+  let bb = 40
+}
+
+console.log(aa)//30
+console.log(bb)//20
+
+
+
+
+for (var i = 0; i < 3; i++) {}
+
+console.log(i)//3
