49c052bad7ef238eba225282af46ed2d97c1de18
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-03-16 20:41:49 +0600

    function done

---
+++ Level-3/t2/l1.js
@@ -0,0 +1,13 @@
+// function declaration
+function hello(){
+    console.log('hi')
+}
+hello()
+
+// function Expression
+
+const x = function(){
+    console.log('hello')
+}
+x()
+// we cann't access expression function before declaration
