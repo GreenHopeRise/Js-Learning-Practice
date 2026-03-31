88c13d2dbc26b70c00b9c45ddbe5d21f45c4e52b
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-03-31 15:54:54 +0600

    scope doen

---
+++ Level-4/t1.js
@@ -0,0 +1,64 @@
+// Global scope 
+let name = 'khalid'
+function names(){
+    console.log(name)
+}
+names()
+
+
+// Function scope 
+ function fn(){
+    let x = 5
+ }
+// --->  console.log(x) //Error
+
+
+//  block scope means {..}
+// const and let 
+if(true){
+    let y =7
+
+}
+// --->console.log(y) //ref error
+
+if(true){
+    var z =5
+}
+console.log(z)
+
+
+
+// Lexical scope 
+
+let a = 10;
+function outer() {
+  let b = 20;
+
+  function inner() {
+    console.log(a, b);
+  }
+
+  inner();
+}
+
+outer();
+
+
+// Execution context = box 
+//  Global execution context (vairable, function, this keyweor)
+
+// Search chain 
+let ax = 1;
+
+function one() {
+  let b = 2;
+
+  function two() {
+    let c = 3;
+    console.log(ax, b, c);
+  }
+
+  two();
+}
+
+one();
