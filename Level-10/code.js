0cabc6f6896a9fb92cd1ecd8a4ef9a53ea5729a7
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-04-09 11:00:38 +0600

    hudai

--- Level-10/code.js
+++ Level-10/code.js
@@ -12,3 +12,18 @@ const d = new Promise ((resolve, reject)=>{
 })
 
 d.then(r => console.log(r))
+
+
+const x = new Promise ( (resolve, reject)=>{
+    let success = false
+    if(success) resolve('good')
+    else reject('bad')    
+})
+x.then(r=>console.log(r)).catch(t=>console.log(t))
+
+
+
+const pro = new Promise((resolve, reject)=>{
+    resolve(2)
+})
+pro.then(r=>r*2).then(r=>r*2).then(r=>r*2).then(r=>console.log(r))
